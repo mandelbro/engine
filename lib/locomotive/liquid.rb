544a559d2e93187dd8388bf785514e8eac2d478e
mandelbro <chrismontes@about.me>
2014-10-14 15:14:10 -0700

    Adds consume to pre-require list

--- lib/locomotive/liquid.rb
+++ lib/locomotive/liquid.rb
@@ -3,6 +3,7 @@ require 'locomotive/liquid/drops/proxy_collection'
 require 'locomotive/liquid/filters/base'
 require 'locomotive/liquid/tags/hybrid'
 require 'locomotive/liquid/tags/path_helper'
+require 'locomotive/liquid/tags/consume'
 
 %w{. tags drops filters}.each do |dir|
   Dir[File.join(File.dirname(__FILE__), 'liquid', dir, '*.rb')].each { |lib| require lib }
